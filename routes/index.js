fe2005d2b7b6e2b82cdb7f5a4d753f8d11e115b0
franaguee <67479979+franaguee@users.noreply.github.com>
2020-09-18 22:43:36 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const controller = require('../controllers/index');
 const router = express.Router();
-const router = require('./routes/index');
-app.use('/', router);
+
 router.get('/', controller.home);
 
 router.get('/search', controller.search)
 
 module.exports = router;
+© 2020 GitHub, Inc.
